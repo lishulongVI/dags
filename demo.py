22567dcef112ab6c7d9b8579f6d9e00222e6fbca
lishulong <lishulong@wecash.net>
2018-10-31 16:47:23 +0800

    demo

---
+++ demo.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from scr.quality.prod import ppt
+from scr.quality.test import pt
+
+args = {
+    'owner': 'lishulong',
+    'start_date': airflow.utils.dates.days_ago(2),
+    'depends_on_past': False,
+}
+
+dag = DAG(
+    dag_id='python_quality', default_args=args,
+    schedule_interval='0 0 * * *'
+
+)
+
+s1 = PythonOperator(
+    task_id='test',
+    provide_context=True,
+    python_callable=pt(),
+    dag=dag)
+
+s2 = PythonOperator(
+    task_id='prod',
+    provide_context=True,
+    python_callable=ppt(),
+    dag=dag)
+
+s1.set_upstream(s2)
